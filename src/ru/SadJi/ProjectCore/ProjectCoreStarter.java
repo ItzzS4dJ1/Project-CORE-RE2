7881d2f7357bd17e1d767d04183fae8e827fa035
ItzzS4dJ1 <145474834+ItzzS4dJ1@users.noreply.github.com>
2025-01-22 11:58:23 +0300

    Update ProjectCoreStarter.java

--- src/ru/SadJi/ProjectCore/ProjectCoreStarter.java
+++ src/ru/SadJi/ProjectCore/ProjectCoreStarter.java
@@ -18,7 +18,6 @@ public class ProjectCoreStarter extends Mod{
             Time.runTask(10f, () -> {
                 BaseDialog dialog = new BaseDialog("frog");
                 dialog.cont.add("behold").row();
-                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
                 dialog.cont.image(Core.atlas.find("pr-core-frog")).pad(20f).row();
                 dialog.cont.button("I see", dialog::hide).size(100f, 50f);
                 dialog.show();
